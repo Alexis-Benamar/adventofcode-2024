b222dc343473052d598ff4ec6aeefe7b64599c57
alexisbenamar <alexis.benamar@sinch.com>
2024-12-10 15:36:21 +0100

    add day 03

---
+++ 03/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+//go:embed input.txt
+var rawData string
+var data string
+
+func init() {
+	rawData = strings.TrimRight(rawData, "\n")
+	if len(rawData) == 0 {
+		panic("empty data file")
+	}
+
+	data = strings.Split(rawData, "\n\n")[0]
+}
+
+func checkMemory(data string, part int8) int {
+	var sum int
+	reMul := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	reAll := regexp.MustCompile(`(don't\(\))|(do\(\)|(mul\((\d{1,3}),(\d{1,3})\)))`)
+
+	if (part == 1) {
+		matches := reMul.FindAllStringSubmatch(data, -1)
+
+		for _, match := range matches {
+			num1, _ := strconv.Atoi(match[1])
+			num2, _ := strconv.Atoi(match[2])
+			sum += num1 * num2
+		}
+	}
+
+	if (part == 2) {
+		var ignoreMul bool
+		ignoreMulMap := map[string]bool{"do()": false,"don't()": true}
+
+		allMatches := reAll.FindAllStringSubmatch(data, -1)
+
+		for _, match := range allMatches {
+			if !strings.HasPrefix(match[0], "mul") {
+				ignoreMul = ignoreMulMap[match[0]]
+				continue
+			}
+
+			if ignoreMul {
+				continue
+			}
+
+			num1, _ := strconv.Atoi(match[4])
+			num2, _ := strconv.Atoi(match[5])
+			sum += num1 * num2
+		}
+	}
+
+	return sum
+}
+
+func main() {
+	start := time.Now()
+
+	part1 := checkMemory(data, 1)
+	part2 := checkMemory(data, 2)
+
+	elapsed := time.Since(start)
+
+	fmt.Printf("part1: %d\n", part1)
+	fmt.Printf("part2: %d\n", part2)
+	fmt.Printf("Execution time %f s\n", elapsed.Seconds())
+}
